80efebea38c3bd7bbaa6dc8911bcb6fb5863e687
amhsm6 <amhsm6@gmail.com>
2025-02-26 09:25:29 +0200

    feat: pid for wall movement

--- main.py
+++ main.py
@@ -10,30 +10,34 @@ camera = Camera()
 eyes = Eyes(i2c)
 wheels = Wheels()
 
-KP = 40
-KD = 0
+KP = 35
+KI = 5
+KD = 13
 SPEED = 20000
+DT = 0.01
 
 try:
+    err_old = 0
+    err_sum = 0
     while True:
-        #dist = eyes.see(0)
-        #err = dist - 200
+        dist = eyes.see(0)
+        err = dist - 150
 
-        #power = err * KP
-        #print(dist)
-        #print(SPEED + power, SPEED - power)
+        err_sum += err
+        control = err * KP + err_sum * DT * KI + (err - err_old) / DT * KD
+        err_old = err
 
-        #wheels.go(SPEED + power, SPEED - power)
+        wheels.go(SPEED + control, SPEED - control)
+        time.sleep(DT)
 
-        im = camera.read()
-        for row in range(24):
-            for col in range(32):
-                print(f'{im[row, col]:.2f}', end=' ')
+        #im = camera.read()
+        #for row in range(24):
+        #    for col in range(32):
+        #        print(f'{im[row, col]:.2f}', end=' ')
 
-            print()
+        #    print()
 
-        print('=' * 15)
-        time.sleep(1)
+        #print('=' * 15)
 
 except KeyboardInterrupt:
     print()
